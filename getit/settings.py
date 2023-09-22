5d2ebb41d3b2219fd576f14aecc1bff72967a41b
theodecourt <theod@al.insper.edu.br>
2023-09-22 16:11:59 -0300

    modificacao para deploy

--- getit/settings.py
+++ getit/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-19%c2)njyx@x*bqea81yurpzt2m#5#5uh83hq=*72ul9fhq8*3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -75,14 +76,11 @@ WSGI_APPLICATION = 'getit.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'getit',
-        'USER': 'getituser',
-        'PASSWORD': 'getitsenha',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.config(
+        default='PGPASSWORD=VWjBsqmQ1umwUfg6dOBP psql -h containers-us-west-119.railway.app -U postgres -p 6388 -d railway',
+        conn_max_age=600,
+        ssl_require=not DEBUG
+    )
 }
 
 
